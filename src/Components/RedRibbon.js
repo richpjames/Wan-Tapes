ae89ce149fcaa047a25e54720501d02dd92147c7
ricojamjar <rich@genericgreeting.co.uk>
2019-07-25 18:34:04 +0100

    converting css units vh/vw to %

--- src/Components/RedRibbon.js
+++ src/Components/RedRibbon.js
@@ -2,8 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const RibbonWrapper = styled.section`
-  width: 100vw;
-  height: 15vh;
+  width: 100%;
+  height: 15%;
   background: #ff3030;
   z-index: 10;
   position: absolute;
