643609be8a1f921f48b149de313514eeeb58f7b4
ricojamjar <rich@genericgreeting.co.uk>
2019-07-26 09:49:56 +0100

    changing buy to listen...

--- src/Components/Buy.js
+++ src/Components/Buy.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const BuyWrapper = styled.div`
+const BuyWrapper = styled.h1`
   position: absolute;
   bottom: 1%;
   right: 1%;
@@ -10,28 +10,7 @@ const BuyWrapper = styled.div`
 export default function Buy() {
   return (
     <BuyWrapper>
-      <form
-        action="https://www.paypal.com/cgi-bin/webscr"
-        method="post"
-        target="_top"
-      >
-        <input type="hidden" name="cmd" value="_s-xclick" />
-        <input type="hidden" name="hosted_button_id" value="8PLDYY7R9GCBN" />
-        <input
-          type="image"
-          src="http://www.wantapes.com/img/buy.png"
-          border="0"
-          name="submit"
-          alt="PayPal – The safer, easier way to pay online!"
-        />
-        <img
-          alt=""
-          border="0"
-          src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif"
-          width="1"
-          height="1"
-        />
-      </form>
+      Listen...
     </BuyWrapper>
   );
 }
