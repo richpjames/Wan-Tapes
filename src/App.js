06853bf72398b010c96b20f540552d184007f078
ricojamjar <rich@genericgreeting.co.uk>
2019-07-29 18:52:11 +0100

    adding routing to home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import Container from "./Components/Container"
-import RedRibbon from "./Components/RedRibbon"
-import Background from "./Components/Background"
-import './App.css';
+import React from "react";
+import { Router } from "@reach/router";
+import HomePage from "./Components/home/HomePage";
+import HotTapHome from "./Components/hot-tap/HotTapHome";
+import RedRibbon from "./Components/RedRibbon";
+import Background from "./Components/Background";
+import Container from "./Components/Container";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Background />
       <Container />
+      <Router>
+        <HomePage path="/" />
+        <HotTapHome path="/releases/hot-tap" />
+      </Router>
+      <Background />
       <RedRibbon />
     </div>
   );
