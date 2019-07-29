95f0863ecdc26b97c9bf6d262f246710b10610f9
ricojamjar <rich@genericgreeting.co.uk>
2019-07-29 18:53:37 +0100

    Adding routing

--- src/Components/home/Listen.js
+++ src/Components/home/Listen.js
@@ -1,16 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "@reach/router";
 
 const ListenWrapper = styled.h1`
   position: absolute;
-  bottom: 1%;
-  right: 1%;
+  bottom: 6%;
+  right: 20%;
+  z-index: 10;
 `;
 
 export default function Listen() {
   return (
     <ListenWrapper>
-      Listen...
+      <Link to="/releases/hot-tap">Listen...</Link>
     </ListenWrapper>
   );
 }
