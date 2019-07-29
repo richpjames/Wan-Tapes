0b03fd1e595827b2be2fc6f259c8d7ff1910cb36
ricojamjar <rich@genericgreeting.co.uk>
2019-07-29 19:16:21 +0100

    adding functionality to play tracks

--- src/Components/hot-tap/AudioPlayer.js
+++ src/Components/hot-tap/AudioPlayer.js
@@ -4,9 +4,24 @@ export default class AudioPlayer extends Component {
   state = {
     selectedTrack: null,
     tracks: [
-      { id: "A1", title: "Lane Creeper", length: 5.39, uri: "https://www.wantapes.com/trax/A1-Lane-Creeper.mp3" },
-      { id: "A2", title: "Gooseberry", length: 4.47, uri: "https://www.wantapes.com/trax/A2-Gooseberry.mp3" },
-      { id: "B1", title: "Almost Definitely Nothing", length: 12.01, uri: "https://www.wantapes.com/trax/B1-Almost-Definitely-Nothing.mp3" }
+      {
+        id: "A1",
+        title: "Lane Creeper",
+        length: 5.39,
+        uri: "https://www.wantapes.com/trax/A1-Lane-Creeper.mp3"
+      },
+      {
+        id: "A2",
+        title: "Gooseberry",
+        length: 4.47,
+        uri: "https://www.wantapes.com/trax/A2-Gooseberry.mp3"
+      },
+      {
+        id: "B1",
+        title: "Almost Definitely Nothing",
+        length: 12.01,
+        uri: "https://www.wantapes.com/trax/B1-Almost-Definitely-Nothing.mp3"
+      }
     ]
   };
   render() {
@@ -24,13 +39,35 @@ export default class AudioPlayer extends Component {
     return (
       <div>
         <ul>{tracklist}</ul>
-        <audio ref={ref => this.player = ref} />
+        <audio ref={ref => (this.player = ref)} />
       </div>
     );
   }
-  componentDidUpdate(prevProps, prevState){
-    if(this.state.selectedTrack !== prevState.selectedTrack){
-      
+  componentDidUpdate(prevProps, prevState) {
+    const { selectedTrack } = this.state;
+    const laneCreeperLink = this.state.tracks[0].uri;
+    const gooseberryLink = this.state.tracks[1].uri;
+    const almostDefLink = this.state.tracks[2].uri;
+    let { src, play } = this.player;
+    if (selectedTrack !== prevState.selectedTrack) {
+      let trackToPlay;
+      switch (selectedTrack) {
+        case "Lane Creeper":
+          trackToPlay = laneCreeperLink;
+          break;
+        case "Gooseberry":
+          trackToPlay = gooseberryLink;
+          break;
+        case "Almost Definitely Nothing":
+          trackToPlay = almostDefLink;
+          break;
+        default:
+          break;
+      }
+      if (trackToPlay) {
+        src = trackToPlay;
+        play();
+      }
     }
   }
 }
