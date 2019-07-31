8aa3aa90dea0bbd93a111efaf2bee90734ce5418
ricojamjar <rich@genericgreeting.co.uk>
2019-07-31 14:21:16 +0100

    adding tests

--- cypress/integration/wan-tapes_spec.js
+++ cypress/integration/wan-tapes_spec.js
@@ -1,5 +1,9 @@
 describe("home page", () => {
-  it("Visits the home page", () => {
+  it("Navigates to Hot Tap page", () => {
     cy.visit("http://localhost:3000/");
+
+    cy.contains("Listen...").click();
+
+    cy.url().should("include", "/releases/hot-tap");
   });
 });
