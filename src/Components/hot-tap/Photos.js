c8f0bc2107f964e5d5273b9e7e9941f00f7cc5e3
ricojamjar <rich@genericgreeting.co.uk>
2019-08-02 15:35:23 +0100

    adding lightbox

--- src/Components/hot-tap/Photos.js
+++ src/Components/hot-tap/Photos.js
@@ -1,32 +1,71 @@
 import React, { Component } from "react";
-import Img from "react-image";
 import styled from "styled-components";
 import VisibilitySensor from "react-visibility-sensor";
+import ImageZoom from "react-medium-image-zoom";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 
-const Image = styled.div`
-  height: 50px;
-  width: 50px;
-
-  position: relative;
+const ImagesWrapper = styled.div`
+  display: flex;
+  padding-top: 10%;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  height: 80%;
+  z-index: 5;
 `;
+const Image = styled.img`
+  max-width: 50px;
+  z-index: 20;
+`;
+const baseUrl = "https://www.wantapes.com/img/TapeImage";
+const photos = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 
+const imagesUrls = photos.map(photo => `${baseUrl}${photo}`);
 export default class Photos extends Component {
-  state = {
-    baseUrl: "https://www.wantapes.com/img/TapeImage",
-    photos: ["1.jpg", "2.jpg", "3.jpg", "4.jpg"]
-  };
+  state = { photoIndex: 0, isOpen: false };
 
   render() {
-    const { photos, baseUrl } = this.state;
-    const photoReel = photos.map(photo => {
-      return (
-        <VisibilitySensor>
-          <Image>
-            <Img src={`${baseUrl}${photo}`} alt="photo of Hot Tape" />
-          </Image>
-        </VisibilitySensor>
-      );
+    const { photoIndex, isOpen } = this.state;
+    const photoReel = imagesUrls.map(photo => {
+      return <Image className="Image" src={photo} key={photo} />;
     });
-    return photoReel;
+    return (
+      <div>
+        <button type="button" onClick={() => this.setState({ isOpen: true })}>
+          Open Lightbox
+        </button>
+        {isOpen && (
+          <Lightbox
+            mainSrc={imagesUrls[photoIndex]}
+            nextSrc={imagesUrls[(photoIndex + 1) % imagesUrls.length]}
+            prevSrc={
+              imagesUrls[
+                (photoIndex + imagesUrls.length - 1) % imagesUrls.length
+              ]
+            }
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex:
+                  (photoIndex + imagesUrls.length - 1) % imagesUrls.length
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % imagesUrls.length
+              })
+            }
+          />
+        )}
+        <VisibilitySensor>
+          <ImagesWrapper>{photoReel}</ImagesWrapper>
+        </VisibilitySensor>
+      </div>
+    );
   }
+  enlarge = () => {
+    this.setState({ isOpen: true });
+    alert("dong");
+  };
 }
