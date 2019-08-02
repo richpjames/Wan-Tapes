6ad55dd74c5539c6d01d3642e5ee840c3b03f7af
ricojamjar <rich@genericgreeting.co.uk>
2019-08-02 10:17:19 +0100

    changing name of button wrapper

--- src/Components/hot-tap/MusicControlButton.js
+++ src/Components/hot-tap/MusicControlButton.js
@@ -2,15 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import { FaStop, FaPause } from "react-icons/fa";
 
-const ButtonWrapper = styled.button``;
+export const PlayStopPause = styled.button`
+  margin-top: 5px;
+`;
 
 export default function MusicControlButton({ playState, setPlayState }) {
   return (
-    <ButtonWrapper
+    <PlayStopPause
       className="ButtonWrapper"
       onClick={() => setPlayState(playState)}
     >
       {playState === "Stop" ? <FaStop /> : <FaPause />}
-    </ButtonWrapper>
+    </PlayStopPause>
   );
 }
