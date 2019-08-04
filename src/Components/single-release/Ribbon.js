119c4adf87e55027a9b1c2511f37a3a22251abdc
ricojamjar <rich@genericgreeting.co.uk>
2019-08-04 22:57:37 +0100

    moving files to single-release folder

--- src/Components/single-release/Ribbon.js
+++ src/Components/single-release/Ribbon.js
@@ -4,12 +4,12 @@ import styled from "styled-components";
 const RibbonWrapper = styled.section`
   width: 100%;
   height: 15%;
-  background: #ff3030;
+  background: ${props => (props.colors ? props.colors.other : "#000")};
   z-index: 10;
   position: absolute;
   top: 35%;
 `;
 
-export default function Ribbon() {
-  return <RibbonWrapper />;
+export default function Ribbon(props) {
+  return <RibbonWrapper {...props} />;
 }
