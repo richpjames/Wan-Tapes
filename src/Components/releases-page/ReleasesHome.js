383e07d4123bb212be09ae074abfd2479dee05f8
ricojamjar <rich@genericgreeting.co.uk>
2019-08-04 22:58:27 +0100

    passing props

--- src/Components/releases-page/ReleasesHome.js
+++ src/Components/releases-page/ReleasesHome.js
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { releases } from "../../data/releases.json";
 import AudioPlayer from "../single-release/AudioPlayer";
-import BackgroundWrapper from "../Background";
-import Container from "../Container";
+import BackgroundWrapper from "../single-release/Background";
+import Container from "../single-release/Container";
 import TitleText from "../single-release/TitleText";
 import Photos from "../single-release/Photos";
 import BuyButton from "../single-release/BuyButton";
-import Ribbon from "../Ribbon"
+import Ribbon from "../single-release/Ribbon";
 
 const SecondContainerWrap = styled.div`
   margin-top: 15vh;
 `;
 
-const Release = styled.section`width:20%;`;
+const Release = styled.section`
+  width: 20%;
+`;
 
 const ReleasesWrapper = styled.section``;
 
@@ -26,17 +28,26 @@ export default class ReleasesHome extends Component {
   render() {
     const { releases } = this.state;
     const releaseRoll = releases.map(
-      ({ title, artist, releaseDate, tracks, buyCode, photos, url, colors }) => {
+      ({
+        title,
+        artist,
+        releaseDate,
+        tracks,
+        buyCode,
+        photos,
+        baseUrl,
+        colors
+      }) => {
         return (
           <Release key={releaseDate}>
-            <BackgroundWrapper colors={colors} height={"200vh"}/>
+            <BackgroundWrapper colors={colors} height={"200vh"} />
             <Container primary />
             <TitleText title={title} artist={artist} />
-            <AudioPlayer tracks={tracks} url ={url}/>
-            <Ribbon colors={colors}/>
+            <AudioPlayer tracks={tracks} baseUrl={baseUrl} />
+            <Ribbon colors={colors} />
             <SecondContainerWrap>
               <Container primary />
-              <Photos photos={photos} url={url}/>
+              <Photos photos={photos} baseUrl={baseUrl} />
             </SecondContainerWrap>
             <BuyButton buyCode={buyCode} />
           </Release>
