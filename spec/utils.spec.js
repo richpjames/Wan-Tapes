df5d54b953dda2ea5c76c044aa23b2132479647c
ricojamjar <rich@genericgreeting.co.uk>
2019-08-05 15:41:23 +0100

    adding test for three items and clarifying it statement

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -38,7 +38,7 @@ describe("AddLineBreak", () => {
     const expectedOutput = ["NoSpace", "NoSpace"];
     expect(actualOutput).to.eql(expectedOutput);
   });
-  it("returns one 'NoSpace' and one 'Space' in array when passed two track object with two different first letters of id", () => {
+  it("returns ['NoSpace', 'Space'] when passed two track object with two different first letters of id", () => {
     const input = [
       {
         id: "A1",
@@ -49,6 +49,34 @@ describe("AddLineBreak", () => {
       },
       {
         id: "B2",
+        title: "Gooseberry",
+        length: 4.47,
+        glyph: "†",
+        uri: "https://www.wantapes.com/releases/ht-ht/trax/A2-Gooseberry.mp3"
+      }
+    ];
+    const actualOutput = AddLineBreak(input);
+    const expectedOutput = ["NoSpace", "Space"];
+    expect(actualOutput).to.eql(expectedOutput);
+  });
+  it("returns ['NoSpace', 'Space', 'NoSpace']in array when passed three track object with two different first letters of id followed by one of the same id", () => {
+    const input = [
+      {
+        id: "A1",
+        title: "Lane Creeper",
+        length: 5.39,
+        glyph: "†",
+        uri: "https://www.wantapes.com/releases/ht-ht/trax/A1-Lane-Creeper.mp3"
+      },
+      {
+        id: "B2",
+        title: "Gooseberry",
+        length: 4.47,
+        glyph: "†",
+        uri: "https://www.wantapes.com/releases/ht-ht/trax/A2-Gooseberry.mp3"
+      },
+       {
+        id: "B3",
         title: "Gooseberry",
         length: 4.47,
         glyph: "†",
