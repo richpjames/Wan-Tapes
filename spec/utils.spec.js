34c5b79d15fb3ddce65a794b5bc501cb3925b2bf
ricojamjar <rich@genericgreeting.co.uk>
2019-08-05 15:38:57 +0100

    adding test for two different side ids

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -38,4 +38,25 @@ describe("AddLineBreak", () => {
     const expectedOutput = ["NoSpace", "NoSpace"];
     expect(actualOutput).to.eql(expectedOutput);
   });
+  it("returns one 'NoSpace' and one 'Space' in array when passed two track object with two different first letters of id", () => {
+    const input = [
+      {
+        id: "A1",
+        title: "Lane Creeper",
+        length: 5.39,
+        glyph: "†",
+        uri: "https://www.wantapes.com/releases/ht-ht/trax/A1-Lane-Creeper.mp3"
+      },
+      {
+        id: "B2",
+        title: "Gooseberry",
+        length: 4.47,
+        glyph: "†",
+        uri: "https://www.wantapes.com/releases/ht-ht/trax/A2-Gooseberry.mp3"
+      }
+    ];
+    const actualOutput = AddLineBreak(input);
+    const expectedOutput = ["NoSpace", "Space"];
+    expect(actualOutput).to.eql(expectedOutput);
+  });
 });
--- utils/utils.js
+++ utils/utils.js
@@ -1,10 +1,14 @@
 const AddLineBreak = tracks => {
   const lineBreaks = tracks.map((track, i) => {
-    // let indexMinus1 = i - 1;
-    // console.log(tracks[indexMinus1]);
-    return "NoSpace";
+    let indexMinus1 = i - 1;
+    let previousTrackSideId = tracks[indexMinus1]
+      ? tracks[indexMinus1].id[0]
+      : tracks[i].id[0];
+    let currentTrackSideId = tracks[i].id[0];
+    if (previousTrackSideId !== currentTrackSideId) return "Space";
+    else return "NoSpace";
   });
-  console.log(lineBreaks)
+
   return lineBreaks;
 };
 
