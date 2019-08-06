bab8f05932b6c1c64742d51a37b9869a3b50ab8f
ricojamjar <rich@genericgreeting.co.uk>
2019-08-06 11:23:37 +0100

    adding splash image to homepage

--- src/Components/home/HomePage.js
+++ src/Components/home/HomePage.js
@@ -1,10 +1,45 @@
 import React from "react";
-import Background from "../single-release/ReleaseWrap";
+import styled from "styled-components";
+import wansplash from "../../Assets/wan-splash.png";
+
+const Container = styled.section`
+  height: 100vh;
+  max-height: 100%;
+  max-width: 100%;
+  width: 100vw;
+  margin: 0 auto;
+  /* background: red; */
+  z-index: -1;
+  overflow: hidden;
+`;
+
+const WanTapesLetters = styled.img`
+  height: auto;
+  width: 80%;
+  position: absolute;
+  z-index: 5;
+  background: white;
+  overflow: hidden;
+  margin-left: -40%;
+  margin-top: -50%;
+`;
+
+const ImageWrap = styled.div`
+  overflow: hidden;
+  margin: 0 auto;
+  width: 80%;
+  background: url(./../../Assets/wan-splash.png) center center no-repeat;
+`;
 
 export default function HomePage() {
   return (
-    <>
-      <Background />
-    </>
+    <Container className="Container">
+      <ImageWrap className="ImageWrap" />
+      {/* <WanTapesLetters
+          className="WanTapesLetters"
+          src={wansplash}
+          alt="wan tapes splash"
+        /> */}
+    </Container>
   );
 }
