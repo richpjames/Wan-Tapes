36bf9bf8f60178fcbc53c57e1a006c8388377aac
ricojamjar <rich@genericgreeting.co.uk>
2019-08-06 11:24:38 +0100

    removing conditional logic

--- src/Components/single-release/StopButton.js
+++ src/Components/single-release/StopButton.js
@@ -2,17 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import { FaStop, FaPause } from "react-icons/fa";
 
-export const PlayStopPause = styled.button`
+export const Stop = styled.button`
   margin-top: 5px;
 `;
 
-export default function MusicControlButton({ playState, setPlayState }) {
+export default function MusicControlButton({ setPlayState }) {
   return (
-    <PlayStopPause
-      className="ButtonWrapper"
-      onClick={() => setPlayState(playState)}
-    >
-      {playState === "Stop" ? <FaStop /> : <FaPause />}
-    </PlayStopPause>
+    <Stop className="ButtonWrapper" onClick={() => setPlayState("Stop")}>
+      <FaStop />
+    </Stop>
   );
 }
