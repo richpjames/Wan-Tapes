734ad35715463bb9bc28109b7b57c9fc5d773822
ricojamjar <rich@genericgreeting.co.uk>
2019-08-09 10:12:21 +0100

    creating timeline component

---
+++ src/Components/home/Timeline.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const TimelineRibbon = styled.div`
+  height: 10vh;
+  width: 200vw;
+  background: #000;
+  top: 35%;
+  position: relative;
+`;
+
+export default function Timeline() {
+  return <TimelineRibbon />;
+}
