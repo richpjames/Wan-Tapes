e71ec2022ea5a8d6fd36610cca3f279c853e3352
ricojamjar <rich@genericgreeting.co.uk>
2019-08-19 19:32:05 +0100

    moving CoverArt to /single-release and deleting duplicate component

--- src/Components/single-release/CoverArt.js
+++ src/Components/single-release/CoverArt.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
-const CoverArt = styled.img`
+const Cover = styled.img`
   height: 100%;
   width: 100%;
   object-fit: cover;
 `;
 
 const CoverArt = props => {
-  const { coverArt } = props;
-  return <Cover src={coverArt} />;
+  const { coverArt, showDetail } = props;
+  return <Cover src={coverArt} onClick={showDetail} />;
 };
 
 export default CoverArt;
