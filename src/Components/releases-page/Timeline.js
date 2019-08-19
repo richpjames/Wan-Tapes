6b07ac1a7e7cf77061e161ae4d325abe244d1b0d
ricojamjar <rich@genericgreeting.co.uk>
2019-08-19 18:50:32 +0100

    adjusting timeline styling

--- src/Components/releases-page/Timeline.js
+++ src/Components/releases-page/Timeline.js
@@ -2,11 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const TimelineRibbon = styled.div`
-  height: 10vh;
+  height: 5vh;
   width: 200vw;
   background: #000;
   top: 35%;
-  position: relative;
+  position: absolute;
 `;
 
 export default function Timeline() {
